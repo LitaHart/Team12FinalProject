b0963950778efbabf086e70676d21297ecb74109
yoky <leeyoky@naver.com>
2022-08-03 10:11:59 +0900

    [MODIFY]index.jsp/home.jsp/HC

--- 01_Team1Final/src/main/java/com/team12/main/HomeController.java
+++ 01_Team1Final/src/main/java/com/team12/main/HomeController.java
@@ -1,13 +1,9 @@
 package com.team12.main;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,11 +11,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(HttpServletRequest req) {
 		
+		req.setAttribute("contentPage", "home.jsp");
+		return "index";
+	}
+	
+	@RequestMapping(value = "/index.go", method = RequestMethod.GET)
+	public String indexGo(HttpServletRequest req) {
 		
-		return "home";
+		return home(req);
 	}
 	
 }
