af79f46ec42948ab2ba0c1d8c7c643e015db06a7
Lim DongHoon <dla_ehdgns@naver.com>
2022-08-05 11:38:11 +0900

    습관적 커밋

--- FinalProject/src/main/java/com/team12/main/team1/join/MemberController.java
+++ FinalProject/src/main/java/com/team12/main/team1/join/MemberController.java
@@ -67,12 +67,12 @@ public class MemberController {
 		
 		// 세션이 없을 때 null인데 그걸 잘라달라하면 500에러
 		mDAO.splitAddr(req);
-		req.setAttribute("contentPage", "LDH/info.jsp");
+		req.setAttribute("contentPage", "../LDH/info.jsp");
 		
 		} else {
-			req.setAttribute("contentPage", "home.jsp");
+			req.setAttribute("contentPage", "t1_home.jsp");
 		}
-		return "index";
+		return "1Team/t1_index";
 	}
 	
 	
